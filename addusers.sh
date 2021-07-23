bd2d3e9ac97d2a235711561b41e9755a690f8cff
jackey <jackeyli@live.cn>
2021-07-23 14:07:02 +0800

    multi-login

--- addusers.sh
+++ addusers.sh
@@ -6,7 +6,8 @@ for line in `cat userlist`; do
   test -z "$line" && continue
   user=`echo $line | cut -f 1 -d' '`
   echo "existing user $user"
- #  useradd -m -s /bin/bash $user
-#   cp -r /srv/ipython/examples /home/$user/examples
-#   chown -R $user /home/$user/examples
+   useradd -m -s /bin/bash $user
+   echo  "111111\n111111" | passwd $user 
+   cp -r /srv/ipython/examples /home/$user/examples
+   chown -R $user /home/$user/examples
 done
